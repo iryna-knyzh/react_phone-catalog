878805acac192ec6a6b6b936a721ffb7a9ca82f2
Iryna Knyzh <iryna.knyzh@gmail.com>
2026-01-23 14:32:32 +0200

    All from develop in 1 commit

---
+++ src/modules/shared/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -0,0 +1,85 @@
+import { Link, NavLink } from 'react-router-dom';
+import styles from './BurgerMenu.module.scss';
+import { useEffect } from 'react';
+import { asset } from '../../../utils/asset';
+
+type Props = {
+  isBurgerMenuOpen: boolean;
+  onClose: () => void;
+};
+
+export const BurgerMenu: React.FC<Props> = ({ isBurgerMenuOpen, onClose }) => {
+  const handleNavigate = () => {
+    onClose();
+  };
+
+  useEffect(() => {
+    if (isBurgerMenuOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+
+    return () => document.body.classList.remove('no-scroll');
+  }, [isBurgerMenuOpen]);
+
+  return (
+    <div
+      className={
+        isBurgerMenuOpen
+          ? `${styles.burgerMenu} ${styles.isOpen}`
+          : styles.burgerMenu
+      }
+    >
+      <div className={styles.nav}>
+        <NavLink to="/" className={styles.navLink} onClick={handleNavigate}>
+          Home
+        </NavLink>
+        <NavLink
+          to={{ pathname: '/phones' }}
+          className={styles.navLink}
+          onClick={handleNavigate}
+        >
+          Phones
+        </NavLink>
+        <NavLink
+          to={{ pathname: '/tablets' }}
+          className={styles.navLink}
+          onClick={handleNavigate}
+        >
+          Tablets
+        </NavLink>
+        <NavLink
+          to={{ pathname: '/accessories' }}
+          className={styles.navLink}
+          onClick={handleNavigate}
+        >
+          Accessories
+        </NavLink>
+      </div>
+
+      <div className={styles.icons}>
+        <Link
+          to={{ pathname: '/favorites' }}
+          className={styles.iconsLink}
+          onClick={handleNavigate}
+        >
+          <img
+            src={asset('/img/icons/heart.png')}
+            className={styles.iconImg}
+          ></img>
+        </Link>
+        <Link
+          to={{ pathname: '/cart' }}
+          className={styles.iconsLink}
+          onClick={handleNavigate}
+        >
+          <img
+            src={asset('/img/icons/shopping-cart.png')}
+            className={styles.iconImg}
+          ></img>
+        </Link>
+      </div>
+    </div>
+  );
+};
